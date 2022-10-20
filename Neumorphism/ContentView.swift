3ac86a66f5415504b9734b595743540bc6f08839
Leehan Choi <devprworld@gmail.com>
2022-10-20 22:17:22 +0900

    Initial Commit

---
+++ Neumorphism/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Neumorphism
+//
+//  Created by IHAN on 2022/10/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Neumorphism/NeumorphismApp.swift
@@ -0,0 +1,17 @@
+//
+//  NeumorphismApp.swift
+//  Neumorphism
+//
+//  Created by IHAN on 2022/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NeumorphismApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
